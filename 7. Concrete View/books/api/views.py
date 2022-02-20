26fd189fabb62a8d3fe099192df62cac9e3270fa
Göksel Kınalı <carryvayne12211@gmail.com>
2022-02-20 23:51:32 +0300

    Added relationship  to concrete api views.

---
+++ 7. Concrete View/books/api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import get_object_or_404
+from books.api.serializers import CommentSerializer,BookSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin,CreateModelMixin
+from books.models import Book,Comment
+from rest_framework import generics
+
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer 
+    
+    
+    
+
+class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer 
+
+
+
+
+class CreateCommentAPIView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    def perform_create(self,serializer):
+        book_id=self.kwargs['book_id']
+        book=get_object_or_404(Book,id=book_id)     
+        serializer.save(book=book)
